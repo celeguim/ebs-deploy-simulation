0b06360b3af37ec23f201b50d4d10b084884b41e
Luiz Celeghin <luiz.celeguim@gmail.com>
2026-02-16 21:32:49 -0300

    xxx

--- ci/detect_objects.py
+++ ci/detect_objects.py
@@ -8,10 +8,11 @@ print("Detecting objects")
 for file in os.listdir("sql"):
     sql = open(f"sql/{file}").read().upper()
     print('File: ', sql)
-    
+
     matches = re.findall(r"(TABLE|PACKAGE|VIEW)\s+(XX\w+)", sql)
     objects.extend(matches)
 
 print("Impacted objects:")
 for o in set(objects):
     print("-", o[0], o[1])
+
